0cfe0a2e735d0124b06a8c2ffb438dca8ec7922f
Niklas <niklasheimburger@gmail.com>
2022-08-16 19:28:34 +0200

    add button with arrow to ShoppingCart-Page + clicking the button routes back to the ProductPage

--- src/pages/shoppingcart.js
+++ src/pages/shoppingcart.js
@@ -1,5 +1,17 @@
+import Link from 'next/link';
+
+import SVG from '../../public/SVG/svg';
 import Header from '../components/Header';
 
 export default function ShoppingCartPage() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Link href="/">
+				<a>
+					<SVG size="40px" color="black" variant="backArrow" />
+				</a>
+			</Link>
+		</>
+	);
 }
