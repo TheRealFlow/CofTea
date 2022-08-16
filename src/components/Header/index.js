a2f08fa98de8a474137396d43707300feb71b47c
Niklas <niklasheimburger@gmail.com>
2022-08-16 16:37:30 +0200

    add routing to shoppingCart-Icon (to empty shoppingCart-Page)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import SVG from '../../../public/SVG/svg';
 
 import StyledHeader from './styled';
@@ -6,7 +8,11 @@ export default function Header() {
 	return (
 		<StyledHeader>
 			<h1>COFTEA</h1>
-			<SVG size="40px" color="lightgreen" variant="cart" />
+			<Link href="./shoppingcart">
+				<a>
+					<SVG size="40px" color="lightgreen" variant="cart" />
+				</a>
+			</Link>
 		</StyledHeader>
 	);
 }
