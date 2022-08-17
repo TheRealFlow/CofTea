85b47227180fd8435b25026b2471652faedbe0be
Niklas <niklasheimburger@gmail.com>
2022-08-17 17:37:27 +0200

    delete needless fractions

--- src/components/ProductList/cart.js
+++ src/components/ProductList/cart.js
@@ -9,26 +9,24 @@ function ProductListCart() {
 	const deleteFromCart = useStore(state => state.deleteFromCart);
 
 	return (
-		<>
-			<StyledProductList>
-				{cartProducts.map(cartProduct => {
-					return (
-						<StyledProductCard key={cartProduct.id}>
-							<p>{cartProduct.name}</p>
-							<p>{cartProduct.price}</p>
-							<StyledButton
-								variant="delete"
-								onClick={() => {
-									deleteFromCart(cartProduct.id);
-								}}
-							>
-								Delete
-							</StyledButton>
-						</StyledProductCard>
-					);
-				})}
-			</StyledProductList>
-		</>
+		<StyledProductList>
+			{cartProducts.map(cartProduct => {
+				return (
+					<StyledProductCard key={cartProduct.id}>
+						<p>{cartProduct.name}</p>
+						<p>{cartProduct.price}</p>
+						<StyledButton
+							variant="delete"
+							onClick={() => {
+								deleteFromCart(cartProduct.id);
+							}}
+						>
+							Delete
+						</StyledButton>
+					</StyledProductCard>
+				);
+			})}
+		</StyledProductList>
 	);
 }
 
