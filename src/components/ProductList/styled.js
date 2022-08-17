a1ae278018abd7df7c28b075e838ea01822a3092
Niklas <niklasheimburger@gmail.com>
2022-08-17 15:35:12 +0200

    changed StyledProductList from 'div' to 'ul'

--- src/components/ProductList/styled.js
+++ src/components/ProductList/styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
-const StyledProductList = styled.div`
+const StyledProductList = styled.ul`
 	display: flex;
 	flex-direction: column;
 	margin: 10px;
 	border: 2px solid black;
 	border-radius: 10px;
+	background-color: blue;
 	list-style: none;
 `;
 
