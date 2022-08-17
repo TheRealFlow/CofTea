478dccda00958f3295573b09bde7f5dbff235b2b
Niklas <niklasheimburger@gmail.com>
2022-08-17 16:15:49 +0200

    add variants to StyledButton

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,37 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const StyledButton = styled.button`
 	margin: 1rem;
-	padding: 0.25rem 0.5rem 0.5rem;
+	padding: 0.25rem 0.5rem;
 	border-radius: 1rem;
 	font-size: 1.25rem;
+
+	${({variant}) =>
+		variant === 'default' &&
+		css`
+			border: 1px solid black;
+			color: black;
+			background-color: none;
+
+			&:active {
+				color: white;
+				background-color: green;
+			}
+		`}
+
+	${({variant}) =>
+		variant === 'added' &&
+		css`
+			color: white;
+			background-color: green;
+		`}
+
+    ${({variant}) =>
+		variant === 'delete' &&
+		css`
+			color: white;
+			background-color: red;
+		`}
 `;
 
 export default StyledButton;
