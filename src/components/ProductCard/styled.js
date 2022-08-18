9f68321946157346586fa593551089d86f43ae17
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-18 18:08:40 +0200

    Add Flexbox to ProductCard

--- src/components/ProductCard/styled.js
+++ src/components/ProductCard/styled.js
@@ -1,34 +1,15 @@
 import styled from 'styled-components';
 
 const StyledProductCard = styled.li`
-	position: relative;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 	width: 90%;
 	margin: 0.5rem;
 	padding: 0.5rem;
 	border: 1px solid black;
 	border-radius: 10px;
 	background-color: lightslategray;
-	h2 {
-		position: absolute;
-		top: 0.25rem;
-		right: 1rem;
-		color: white;
-		color: whitesmoke;
-		font-size: 1.1rem;
-	}
-	h3 {
-		position: absolute;
-		top: 2.25rem;
-		right: 1rem;
-		color: white;
-		font-size: 1rem;
-	}
-	img {
-		position: absolute;
-		top: 0;
-		left: 0;
-		border-radius: 10px;
-	}
 `;
 
 export default StyledProductCard;
