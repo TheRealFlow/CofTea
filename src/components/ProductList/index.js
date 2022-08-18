af4069962ba9ead17b52a5d0bcc6367835be749a
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-18 12:00:28 +0200

    Add Photos to Cards

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import {products} from '../../../backend/db';
 import useStore from '../../hooks/useStore';
 import StyledButton from '../Button';
@@ -13,6 +15,13 @@ export default function ProductList() {
 			{products.map(product => {
 				return (
 					<StyledProductCard key={product.id}>
+						<Image
+							alt={'Photo from the product'}
+							src={product.imgUrl}
+							width={100}
+							height={100}
+							layout="fixed"
+						/>
 						<p>{product.name}</p>
 						<p>{product.price}</p>
 						<StyledButton
