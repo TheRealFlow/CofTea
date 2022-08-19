6709c84197809aebb87ff9a8d5d8897d10903425
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-19 10:04:09 +0200

    fixed styling in shoppingcart

--- src/components/ProductList/cart.js
+++ src/components/ProductList/cart.js
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import useStore from '../../hooks/useStore';
 import StyledButton from '../Button/styled';
 import StyledProductCard from '../ProductCard/styled';
+import StyledProductName from '../ProductName/styled';
+import StyledProductPrice from '../ProductPrice/styled';
 
 import StyledProductList from './styled';
 
@@ -22,8 +24,8 @@ function ShoppingCart() {
 							height={100}
 							layout="fixed"
 						/>
-						<h2>{cartProduct.name}</h2>
-						<h3>{cartProduct.price}</h3>
+						<StyledProductName>{cartProduct.name}</StyledProductName>
+						<StyledProductPrice>{cartProduct.price}</StyledProductPrice>
 						<StyledButton
 							variant="delete"
 							onClick={() => {
