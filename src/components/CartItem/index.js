2cdb5520bbc7d8344f8f2d66907c7b2a29ff99fc
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-22 21:42:35 +0200

    create cartitem component

---
+++ src/components/CartItem/index.js
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+import useStore from '../../hooks/useStore';
+import StyledButton from '../Button/styled';
+import StyledProductName from '../ProductName/styled';
+import StyledProductPrice from '../ProductPrice/styled';
+
+import StyledCartItem from './styled';
+
+export default function CartItem({id, name, price, imgUrl}) {
+	const deleteFromCart = useStore(state => state.deleteFromCart);
+
+	return (
+		<StyledCartItem key={id}>
+			<Image
+				alt={'Photo from the product'}
+				src={imgUrl}
+				width={100}
+				height={100}
+				layout="fixed"
+			/>
+			<StyledProductName>{name}</StyledProductName>
+			<StyledProductPrice>{price}</StyledProductPrice>
+			<StyledButton
+				variant="delete"
+				onClick={() => {
+					deleteFromCart({name});
+				}}
+			>
+				Add To Cart
+			</StyledButton>
+		</StyledCartItem>
+	);
+}
