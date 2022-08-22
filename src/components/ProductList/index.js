ded982f6d50714853ffd98570023a3f0ed4271f0
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-22 21:44:02 +0200

    updated import

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,20 +1,20 @@
-import {products} from '../../../backend/db';
+import useStore from '../../hooks/useStore';
 import ProductCard from '../ProductCard';
 
 import StyledProductList from './styled';
 
 export default function ProductList() {
+	const products = useStore(state => state.products);
 	return (
 		<StyledProductList>
 			{products.map(product => {
 				return (
 					<ProductCard
 						key={product.id}
-						id={product.id}
 						name={product.name}
 						price={product.price}
 						imgUrl={product.imgUrl}
-						product={product}
+						inCart={product.inCart}
 					/>
 				);
 			})}
