072f37b277c066486f3a49524174cd115ab4381e
Niklas <niklasheimburger@gmail.com>
2022-08-24 10:57:10 +0200

    delete unused key=value in database

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,6 @@ export const products = [
 		name: 'Cafe Crema',
 		price: '4,50 €',
 		imgUrl: 'https://images.unsplash.com/photo-1506372023823-741c83b836fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -15,7 +14,6 @@ export const products = [
 		name: 'Green Tea',
 		price: '3,90 €',
 		imgUrl: 'https://images.unsplash.com/photo-1606377695906-236fdfcef767?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -23,7 +21,6 @@ export const products = [
 		name: 'Latte Macchiato',
 		price: '4,90 €',
 		imgUrl: 'https://images.unsplash.com/photo-1599398054066-846f28917f38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -31,7 +28,6 @@ export const products = [
 		name: 'Black Tea',
 		price: '3,90 €',
 		imgUrl: 'https://images.unsplash.com/photo-1627828094454-accc9a7c20e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -39,7 +35,6 @@ export const products = [
 		name: 'Espresso',
 		price: '4,00 €',
 		imgUrl: 'https://images.unsplash.com/photo-1485808191679-5f86510681a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -47,7 +42,6 @@ export const products = [
 		name: 'Rooibos Tea',
 		price: '3,90 €',
 		imgUrl: 'https://images.unsplash.com/photo-1597318181412-49af291f617f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -55,7 +49,6 @@ export const products = [
 		name: 'Black Coffee',
 		price: '3,50 €',
 		imgUrl: 'https://images.unsplash.com/photo-1621267860478-dbdd589372db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -63,7 +56,6 @@ export const products = [
 		name: 'Earl Grey Tea',
 		price: '4,10 €',
 		imgUrl: 'https://images.unsplash.com/photo-1522520788364-9eafb7a93216?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -71,7 +63,6 @@ export const products = [
 		name: 'Iced Mocca',
 		price: '4,90 €',
 		imgUrl: 'https://images.unsplash.com/photo-1517701550927-30cf4ba1dba5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-		inCart: false,
 	},
 	{
 		id: nanoid(),
@@ -79,6 +70,5 @@ export const products = [
 		name: 'Iced Tea - Lime',
 		price: '4,20 €',
 		imgUrl: 'https://images.unsplash.com/photo-1501199532894-9449c0a85a77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-		inCart: false,
 	},
 ];
