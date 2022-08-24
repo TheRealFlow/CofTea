8491935d774cafeca2bb28f3017a5828f7971fbf
Niklas <niklasheimburger@gmail.com>
2022-08-24 14:10:23 +0200

    add counter function (+/-) to useStore

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -2,12 +2,19 @@ import create from 'zustand';
 
 const useStore = create(set => ({
 	cartProducts: [],
+	count: 0,
 	addToCart: id => {
 		set(state => {
 			return {
 				cartProducts: [...state.cartProducts, id],
 			};
 		});
+	},
+	addCount: () => {
+		set(state => ({count: state.count + 1}));
+	},
+	minusCount: () => {
+		set(state => ({count: state.count - 1}));
 	},
 
 	deleteFromCart: id => {
