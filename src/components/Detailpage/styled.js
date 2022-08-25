bf2b1cc1e56035af984b30bb56f2fd5a934ac1ca
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-25 19:15:01 +0200

    create StyledDetailPage

---
+++ src/components/Detailpage/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const StyledDetailPage = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100vw;
+	height: 100vh;
+	margin: 0;
+	padding: 0;
+	background-color: darkgrey;
+	list-style: none;
+	img {
+		border-bottom-right-radius: 20px;
+		border-bottom-left-radius: 20px;
+	}
+`;
+
+export default StyledDetailPage;
