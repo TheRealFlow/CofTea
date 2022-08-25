d466050b9060101b18c52dc454dccf4729aacb44
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-25 19:17:13 +0200

    updated category page

---
+++ src/pages/category/[[...category]].js
@@ -0,0 +1,37 @@
+import {useRouter} from 'next/router';
+
+import {products} from '../../../backend/db';
+import FilterBar from '../../components/FilterBar';
+import Header from '../../components/Header';
+import ProductCard from '../../components/ProductCard';
+import StyledProductList from '../../components/ProductList/styled';
+
+export default function Category() {
+	const router = useRouter();
+	const [filter] = router.query.category ?? [''];
+	return (
+		<>
+			<Header />
+			<FilterBar />
+			<StyledProductList>
+				{products
+					.filter(product => {
+						if (filter.length === 0) {
+							return true;
+						} else {
+							return product.category === filter;
+						}
+					})
+					.map(product => (
+						<ProductCard
+							key={product.id}
+							id={product.id}
+							name={product.name}
+							price={product.price}
+							imgUrl={product.imgUrl}
+						/>
+					))}
+			</StyledProductList>
+		</>
+	);
+}
