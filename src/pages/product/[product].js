4f0257a5a741696dfc4211df7cb1e3c950ff0ec1
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-26 13:17:51 +0200

    addad dynamic productpage

--- src/pages/product/[product].js
+++ src/pages/product/[product].js
@@ -1,44 +1,39 @@
 import Image from 'next/image';
 import {useRouter} from 'next/router';
 
-import {getAllProducts, getProductById} from '../../../backend/db';
+import {getAllProducts} from '../../../backend/db';
 import StyledDetailPage from '../../components/Detailpage/styled';
 import StyledLink from '../../components/Link/styled';
 import StyledProductName from '../../components/ProductName/styled';
 import StyledProductPrice from '../../components/ProductPrice/styled';
 
-export function getStaticPaths() {
-	const products = getAllProducts();
-
-	return {
-		paths: products.map(product => ({params: {id: product.id.toString()}})),
-		fallback: false,
-	};
-}
-
-export function getStaticProps(context) {
-	const product = getProductById(context.params.id);
-
-	return {
-		props: {
-			product,
-		},
-	};
-}
-
 export default function DetailsPage() {
-	const {id, name, price, description, alt, imgUrl} = product;
-	const router = useRouter();
-	const {product} = router.query;
+	const products = getAllProducts();
+	const {query} = useRouter();
 
 	return (
-		<StyledDetailPage key={id}>
-			<Image alt={alt} src={imgUrl} width={100} height={100} layout="fixed" />
-			<StyledProductName>{name}</StyledProductName>
-			<StyledProductPrice>{price}</StyledProductPrice>
-			<p>{description}</p>
-			<StyledLink>Back to Products</StyledLink>
-			<StyledLink>Add to Cart</StyledLink>
+		<StyledDetailPage>
+			{products
+				.filter(product => product.id === query.product)
+				.map(product => {
+					return (
+						<>
+							<Image
+								key={product.id}
+								alt={product.alt}
+								src={product.imgUrl}
+								width={100}
+								height={100}
+								layout="responsive"
+							/>
+							<StyledProductName>{product.name}</StyledProductName>
+							<StyledProductPrice>{product.price}</StyledProductPrice>
+							<p>{product.description}</p>
+							<StyledLink>Back to Products</StyledLink>
+							<StyledLink>Add to Cart</StyledLink>
+						</>
+					);
+				})}
 		</StyledDetailPage>
 	);
 }
