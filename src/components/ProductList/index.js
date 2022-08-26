5bbd86cbaff7980c1744daf31fc1ed808b6958d7
Niklas <niklasheimburger@gmail.com>
2022-08-26 11:56:19 +0200

    refactor ProductList

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,22 +1,14 @@
-import {products} from '../../../backend/db';
+import useStore from '../../hooks/useStore';
 import ProductCard from '../ProductCard';
 
 import StyledProductList from './styled';
 
 export default function ProductList() {
+	const products = useStore(state => state.products);
 	return (
 		<StyledProductList>
 			{products.map(product => {
-				return (
-					<ProductCard
-						key={product.id}
-						id={product.id}
-						name={product.name}
-						price={product.price}
-						imgUrl={product.imgUrl}
-						inCart={product.inCart}
-					/>
-				);
+				return <ProductCard key={product.id} product={product} />;
 			})}
 		</StyledProductList>
 	);
