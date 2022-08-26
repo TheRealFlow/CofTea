cd8ec54c4d3d1c3ba193ffe10b28572109267c3b
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-26 14:25:22 +0200

    create Styled Product Description

---
+++ src/components/ProductDescription/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledProductDescription = styled.p`
+	margin: 1.5rem;
+	color: whitesmoke;
+	font-size: 0.9rem;
+	text-align: center;
+`;
+
+export default StyledProductDescription;
