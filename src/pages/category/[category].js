424ae59eb05c0a20b4d542fe783001cfc23c22a9
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-29 10:12:20 +0200

    add styling to filtered list

--- src/pages/category/[category].js
+++ src/pages/category/[category].js
@@ -1,11 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import {useRouter} from 'next/router';
 
+import SVG from '../../../public/SVG/svg';
+import StyledButton from '../../components/Button/styled';
 import FilterBar from '../../components/FilterBar';
 import Header from '../../components/Header';
+import StyledProductCard from '../../components/ProductCard/styled';
 import StyledProductList from '../../components/ProductList/styled';
+import StyledProductName from '../../components/ProductName/styled';
+import StyledProductPrice from '../../components/ProductPrice/styled';
 import useStore from '../../hooks/useStore';
 
 export default function Category() {
+	const changeQuantity = useStore(state => state.changeQuantity);
 	const products = useStore(state => state.products);
 	const {query} = useRouter();
 	return (
@@ -15,9 +23,34 @@ export default function Category() {
 			<StyledProductList>
 				{products
 					.filter(product => product.category === query.category)
-					.map(product => (
-						<div key={product.id}>{product.name}</div>
-					))}
+					.map(product => {
+						return (
+							<StyledProductCard key={product.id} category={product.category}>
+								<Image
+									alt={product.alt}
+									src={product.imgUrl}
+									width={100}
+									height={100}
+									layout="fixed"
+								/>
+								<StyledProductName>{product.name}</StyledProductName>
+								<StyledProductPrice>{product.price}</StyledProductPrice>
+								<Link href={`/product/${product.id}`} aria-label="Info Button">
+									<a>
+										<SVG size="30px" color="burlywood" variant="info" />
+									</a>
+								</Link>
+								<StyledButton
+									variant="default"
+									onClick={() => {
+										changeQuantity(product.id, 1);
+									}}
+								>
+									Add To Cart
+								</StyledButton>
+							</StyledProductCard>
+						);
+					})}
 			</StyledProductList>
 		</>
 	);
