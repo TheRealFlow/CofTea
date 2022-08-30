73e9bc93d432a962477d6ba6694fecc781490bf0
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-08-30 15:39:35 +0200

    create SelectTable component

---
+++ src/components/SelectTable/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const StyledSelectTable = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	span {
+		margin: 1rem;
+		font-size: 0.9rem;
+	}
+	select {
+		height: 2rem;
+		background-color: burlywood;
+		font-size: 0.9rem;
+	}
+`;
+
+export default StyledSelectTable;
