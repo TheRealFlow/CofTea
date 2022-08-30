d70f26c9aa1c37184bb6c64e33ac3ae8d3733b55
Niklas <niklasheimburger@gmail.com>
2022-08-30 10:21:25 +0200

    create CartBanner Component +add  basic styling

---
+++ src/components/CartBanner/index.js
@@ -0,0 +1,10 @@
+import StyledCartBanner from './styled';
+
+export default function CartBanner() {
+	return (
+		<StyledCartBanner>
+			<h3>totalPrice €</h3>
+			<h3>totalQuantity</h3>
+		</StyledCartBanner>
+	);
+}
---
+++ src/components/CartBanner/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledCartBanner = styled.div`
+	display: flex;
+	justify-content: space-around;
+	margin: 1rem;
+	padding: 0.8rem;
+	border-top: 10px solid #733e32;
+	border-radius: 8px;
+	background-color: #b0d9c6;
+	box-shadow: rgba(0, 0, 0, 0.45) 0 55px 10px -40px;
+`;
+export default StyledCartBanner;
