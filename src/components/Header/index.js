b55e181ac937acde0fe11b5fd74aac0c625b6c85
Niklas <niklasheimburger@gmail.com>
2022-08-31 14:44:47 +0200

    add useRouter + new Header variant + implement new icon variant

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,36 @@
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 
 import SVG from '../../../public/SVG/svg';
 
 import StyledHeader from './styled';
 
 export default function Header() {
+	const router = useRouter();
+	const path = router.asPath;
 	return (
 		<StyledHeader>
-			<Link href="/">
-				<h1>COFTEA</h1>
-			</Link>
-			<Link href="/shoppingcart" aria-label="Shopping Cart">
-				<a>
-					<SVG size="40px" color="burlywood" variant="cart" />
-				</a>
-			</Link>
+			{path === '/' ? (
+				<>
+					<h1>COFTEA</h1>
+
+					<Link href="/shoppingcart" aria-label="Shopping Cart">
+						<a>
+							<SVG size="40px" color="burlywood" variant="cart" />
+						</a>
+					</Link>
+				</>
+			) : (
+				<>
+					<Link href="/" aria-label="Home Page">
+						<a>
+							<SVG size="45px" color="burlywood" variant="home" />
+						</a>
+					</Link>
+
+					<h1>SHOPPING CART</h1>
+				</>
+			)}
 		</StyledHeader>
 	);
 }
