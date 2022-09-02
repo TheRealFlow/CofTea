40a7646bacf66035667bf37a61ae3603317ce6ea
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-09-02 13:45:01 +0200

    changed visibility from Form

--- src/pages/shoppingcart.js
+++ src/pages/shoppingcart.js
@@ -5,8 +5,13 @@ import CartBanner from '../components/CartBanner';
 import ShoppingCart from '../components/CartList';
 import Layout from '../components/Layout';
 import OrderForm from '../components/OrderForm';
+import useStore from '../hooks/useStore';
 
 export default function ShoppingCartPage() {
+	const products = useStore(state => state.products);
+
+	const shoppingCartItems = products.filter(product => product.quantity > 0);
+
 	return (
 		<Layout>
 			<Link href="/" aria-label="Back Arrow">
@@ -15,8 +20,12 @@ export default function ShoppingCartPage() {
 				</a>
 			</Link>
 			<CartBanner />
-			<ShoppingCart />
-			<OrderForm />
+			<ShoppingCart shoppingCartItems={shoppingCartItems} />
+			{shoppingCartItems.length ? (
+				<OrderForm />
+			) : (
+				<span>Please put something in the Shopping Cart first</span>
+			)}
 		</Layout>
 	);
 }
