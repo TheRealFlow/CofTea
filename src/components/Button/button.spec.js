a0b92967910210b315feb5cc023c496621e9073f
Niklas <niklasheimburger@gmail.com>
2022-09-05 10:25:24 +0200

    add click-test

--- src/components/Button/button.spec.js
+++ src/components/Button/button.spec.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+import {userEvent} from '@storybook/testing-library';
 import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 
@@ -13,5 +14,16 @@ describe('Button component', () => {
 		render(<StyledButton>{text}</StyledButton>);
 		const button = screen.getByText(text);
 		expect(button).toBeInTheDocument();
+	});
+
+	describe('Button component', () => {
+		it('should allow clicks', async () => {
+			const handleClick = jest.fn();
+			const text = 'click me';
+			render(<StyledButton onClick={handleClick}>{text}</StyledButton>);
+			const button = screen.getByText(text);
+			await userEvent.click(button);
+			expect(handleClick).toHaveBeenCalledTimes(1);
+		});
 	});
 });
