813bfc33e11962fb8e7ccb03b7cc24217361ab7b
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-09-06 15:25:12 +0200

    create StyledTextarea component

---
+++ src/components/Textarea/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledTextarea = styled.textarea`
+	height: 15vh;
+	padding: 0.5rem;
+	border: 2px solid burlywood;
+	border-radius: 10px;
+	font-size: 0.75rem;
+`;
+
+export default StyledTextarea;
