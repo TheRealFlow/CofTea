2c63c3841859b2888ab462aa8384e74f45c648dc
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-09-06 15:24:53 +0200

    create StyledLabel component

---
+++ src/components/Label/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledLabel = styled.label`
+	color: var(--accent-color);
+	font-size: 1.1rem;
+	letter-spacing: 0.1rem;
+`;
+
+export default StyledLabel;
