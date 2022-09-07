191e6052e1196b552f9c0e9174ae300fe4929109
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-09-07 20:08:09 +0200

    fixeed Productlist position

--- src/components/ProductList/styled.js
+++ src/components/ProductList/styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledProductList = styled.ul`
 	display: flex;
 	position: relative;
-	top: 7rem;
+	top: 7.5rem;
 	flex-direction: column;
 	align-items: center;
 	margin: 0;
