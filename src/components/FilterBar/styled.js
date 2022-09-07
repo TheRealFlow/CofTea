a5a7580ab09a0294254f4569ecc5b92017f2e154
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-09-07 20:27:28 +0200

    fixed positioning

--- src/components/FilterBar/styled.js
+++ src/components/FilterBar/styled.js
@@ -9,7 +9,7 @@ const StyledFilterBar = styled.section`
 	justify-content: space-around;
 	width: 100vw;
 	margin: 0;
-	padding: 0.35rem;
+	padding: 0.5rem 0;
 	border-bottom-right-radius: 10px;
 	border-bottom-left-radius: 10px;
 	background-color: var(--accent-color);
