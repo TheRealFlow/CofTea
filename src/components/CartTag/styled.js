da8d8bd9319b226d0d89e409c1eadb83b1a6da3d
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-09-07 19:19:42 +0200

    fixed cart-banner styling

--- src/components/CartTag/styled.js
+++ src/components/CartTag/styled.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 const StyledCartTag = styled.div`
 	position: relative;
 	margin: 0.8rem;
-	padding: 0.8rem;
+	padding: 0.5rem;
 	border-radius: 10px;
-	background-color: #262626;
-	box-shadow: 2px 2px 10px black;
+	background-color: var(--accent-color);
+	box-shadow: 0 2px 20px rgba(0, 0, 0, 1);
 `;
 export default StyledCartTag;
